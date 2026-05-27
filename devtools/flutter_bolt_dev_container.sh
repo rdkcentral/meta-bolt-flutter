30988d0b5ca573c06e6923ad95edc2bea7db110c
Your Name <you@example.com>
2026-05-27 08:46:29 +0200

    wip 6

--- devtools/flutter_bolt_dev_container.sh
+++ devtools/flutter_bolt_dev_container.sh
@@ -20,20 +20,35 @@ run_in_tmux() {
     else
         is_direct=0
     fi
-    
+
+    if [ "$2" = "ASYNC" ]; then
+        is_async=1
+    else
+        is_async=0
+    fi
+
+
     # 1. Clean up state from any previous commands
     docker exec --user flutter-dev "$CONTAINER_NAME" bash -c 'rm -f /tmp/cmd.out /tmp/cmd.exit'
     
     # 2. Send the command.
     if [ "$is_direct" == "1" ]; then
         # If it's a direct command, we don't wrap it in the exit code capture logic
-        full_command=" ${cmd} > /tmp/cmd.out 2>&1 ; echo "0" > /tmp/cmd.exit"
+        full_command="${cmd} > /tmp/cmd.out 2>&1 ; echo "0" > /tmp/cmd.exit"
+    elif [ "$is_async" == "1" ]; then
+        full_command="${cmd}"
     else
-        full_command=" ( ${cmd} > /tmp/cmd.out 2>&1 ) ; echo \$? > /tmp/cmd.exit"
+        full_command="( ${cmd} > /tmp/cmd.out 2>&1 ) ; echo \$? > /tmp/cmd.exit"
     fi
     
     ${debug} -e "******** Running command in container: \n" ${full_command} "\n********"
     docker exec --user flutter-dev "$CONTAINER_NAME" tmux send-keys -t dev " ${full_command}" ENTER
+
+    if [ "$is_async" == "1" ]; then
+        echo "Command sent to container in async mode. Not waiting for output or exit code."
+        return 0
+    fi
+
     ${debug} "waiting for exit code..."
     
     # 3. Wait (poll) until the exit code file is created
@@ -153,13 +168,13 @@ cmd_push() {
     fi
 
     ${debug} "Checking required environment variables in container..."
-    run_in_tmux 'if [ -z "$FLUTTER_PROJECT_SOURCE_CODE_PATH" ] || [ -z "$FLUTTER_BOLT_NAME" ]; then echo "ERROR: FLUTTER_PROJECT_SOURCE_CODE_PATH and/or FLUTTER_BOLT_NAME are not defined. Run setproject first." >&2; exit 1; fi'
+    run_in_tmux 'if [ -z "$FLUTTER_PROJECT_SOURCE_CODE_PATH" ] || [ -z "$FLUTTER_BOLT_NAME" ] || [ -z "$STB_IP" ]; then echo "ERROR: FLUTTER_PROJECT_SOURCE_CODE_PATH ($FLUTTER_PROJECT_SOURCE_CODE_PATH) and/or FLUTTER_BOLT_NAME ($FLUTTER_BOLT_NAME) and/or STB_IP ($STB_IP) are not defined. Run setproject and setdevice first." >&2; exit 1; fi'
     if [ $? -ne 0 ]; then
         exit 1
     fi
 
     # Execute the push echo output directly from inside the container mapped bash
-    run_in_tmux 'echo "asked to push ${FLUTTER_BOLT_NAME} from ${FLUTTER_PROJECT_SOURCE_CODE_PATH}"'
+    run_in_tmux 'cd /meta-bolt-flutter/bolts; bolt push root@${STB_IP} com.rdkcentral.flutter.app.wonderous+0.1.0'
 }
 
 cmd_debug() {
@@ -169,13 +184,13 @@ cmd_debug() {
     fi
 
     ${debug} "Checking required environment variables in container..."
-    run_in_tmux 'if [ -z "$FLUTTER_PROJECT_SOURCE_CODE_PATH" ] || [ -z "$FLUTTER_BOLT_NAME" ]; then echo "ERROR: FLUTTER_PROJECT_SOURCE_CODE_PATH and/or FLUTTER_BOLT_NAME are not defined. Run setproject first." >&2; exit 1; fi'
+    run_in_tmux 'if [ -z "$FLUTTER_PROJECT_SOURCE_CODE_PATH" ] || [ -z "$FLUTTER_BOLT_NAME" ]; then echo "ERROR: FLUTTER_PROJECT_SOURCE_CODE_PATH ($FLUTTER_PROJECT_SOURCE_CODE_PATH) and/or FLUTTER_BOLT_NAME ($FLUTTER_BOLT_NAME) are not defined. Run setproject first." >&2; exit 1; fi'
     if [ $? -ne 0 ]; then
         exit 1
     fi
 
     # Execute the debug echo output directly from inside the container mapped bash
-    run_in_tmux 'echo "asked to debug ${FLUTTER_BOLT_NAME} from ${FLUTTER_PROJECT_SOURCE_CODE_PATH}"'
+    run_in_tmux 'bolt run root@${STB_IP} com.rdkcentral.flutter.app.wonderous+0.1.0' ASYNC
 }
 
 cmd_stop() {
